5bbc1452398cc98f80aa0945d30369b083478a43
rblistik <31192023+rblistik@users.noreply.github.com>
2017-09-21 01:29:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ WARNING!!! WARNING!!!
 // STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
 // Wrap the entire contents of script.js inside of an IIFE
 // See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
+(Note, Step 2 will be done in the SpeakHello.js file.)
 (function () {
 
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
